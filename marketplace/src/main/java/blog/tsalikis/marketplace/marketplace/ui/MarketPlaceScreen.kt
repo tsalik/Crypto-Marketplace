333fdaace7ec492dd33fa6460b33548700d64456
Kostas Tsalikis <tsalikispk@gmail.com>
2024-11-05 13:54:04 +0200

    Add search functionality (#10)

--- marketplace/src/main/java/blog/tsalikis/marketplace/marketplace/ui/MarketPlaceScreen.kt
+++ marketplace/src/main/java/blog/tsalikis/marketplace/marketplace/ui/MarketPlaceScreen.kt
@@ -1,17 +1,31 @@
 package blog.tsalikis.marketplace.marketplace.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
@@ -21,6 +35,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import blog.tsalikis.marketplace.crypto.design.ShimmerPlaceholder
+import blog.tsalikis.marketplace.marketplace.R
 
 const val marketplaceDestination = "search/tokens"
 
@@ -32,7 +47,10 @@ fun NavGraphBuilder.marketplace() {
             onResume = { viewModel.startPolling() },
             onPause = { viewModel.stopPolling() }
         )
-        MarketPlaceScreen(state)
+        MarketPlaceScreen(
+            state = state,
+            onTextChanged = { viewModel.filterFromSearched(it) }
+        )
     }
 }
 
@@ -63,19 +81,56 @@ fun DisposableLifecycleObserver(onResume: () -> Unit, onPause: () -> Unit) {
 }
 
 @Composable
-fun MarketPlaceScreen(state: MarketPlaceState) {
+fun MarketPlaceScreen(state: MarketPlaceState, onTextChanged: (String) -> Unit) {
     when (state) {
         MarketPlaceState.Error -> Text("Error")
-        MarketPlaceState.Loading -> ShimmerPlaceholder()
+        MarketPlaceState.Loading -> Scaffold { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                ShimmerPlaceholder()
+            }
+        }
+
         is MarketPlaceState.Success -> {
-            MarketplaceContent(state)
+            MarketplaceContent(state, onTextChanged)
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun MarketplaceContent(state: MarketPlaceState.Success) {
-    Scaffold { padding ->
+private fun MarketplaceContent(state: MarketPlaceState.Success, onTextChanged: (String) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    OutlinedTextField(
+                        value = state.query,
+                        onValueChange = {
+                            onTextChanged.invoke(it)
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        placeholder = { Text(stringResource(R.string.search_ticker_placeholder)) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription = stringResource(R.string.search_ticker_placeholder)
+                            )
+                        },
+                        trailingIcon = {
+                            Icon(
+                                Icons.Default.Clear,
+                                contentDescription = stringResource(R.string.clear_search),
+                                modifier = Modifier.clickable { onTextChanged("") }
+                            )
+                        },
+                        singleLine = true
+                    )
+                },
+                modifier = Modifier.padding(top = 8.dp, end = 16.dp)
+            )
+        }
+    ) { padding ->
         LazyColumn(
             contentPadding = padding,
             verticalArrangement = spacedBy(8.dp),
